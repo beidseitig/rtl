8b7d43addf726f28de9667f0129fa3ce89e4a60a
Ramos Fonseca <beidseitig@gmail.com>
2022-03-23 16:45:26 -0300

    Requisito 1 - Testando o componente App

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../components';
+
+describe('Testando o componente About', () => {
+  test('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const headAbout = screen.getByRole('heading', { level: 2, name: /about pokédex/i });
+    expect(headAbout).toBeInTheDocument();
+  });
+
+  test('Testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokePara = screen.getAllByRole('paragraph');
+    expect(pokePara).toHaveLength(2);
+    screen.logTestingPlaygroundURL();
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,46 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente App', () => {
+  test('Testa os links do componente App - Home', () => {
+    renderWithRouter(<App />);
+    const headingPoke = screen.getByRole('heading', { level: 1, name: /Pokédex/i });
+    expect(headingPoke).toBeInTheDocument();
+    screen.logTestingPlaygroundURL();
+
+    const homeLink = screen.queryByRole('link', { name: /home/i });
+    userEvent.click(homeLink);
+
+    const head = screen.getByRole('heading', { level: 2, name: /Encountered pokémons/i });
+    expect(head).toBeInTheDocument();
+  });
+
+  test('Testa os links do componente App - About', () => {
+    renderWithRouter(<App />);
+    const headingPoke = screen.getByRole('heading', { level: 1, name: /Pokédex/i });
+    expect(headingPoke).toBeInTheDocument();
+    screen.logTestingPlaygroundURL();
+
+    const aboutLink = screen.queryByRole('link', { name: /about/i });
+    userEvent.click(aboutLink);
+
+    const head = screen.getByRole('heading', { level: 2, name: /About Pokédex/i });
+    expect(head).toBeInTheDocument();
+  });
+
+  test('Testa os links do componente App - Favorite', () => {
+    renderWithRouter(<App />);
+    const headingPoke = screen.getByRole('heading', { level: 1, name: /Pokédex/i });
+    expect(headingPoke).toBeInTheDocument();
+    screen.logTestingPlaygroundURL();
+
+    const aboutLink = screen.queryByRole('link', { name: /Favorite pokémons/i });
+    userEvent.click(aboutLink);
+
+    const head = screen.getByRole('heading', { level: 2, name: /Favorite pokémons/i });
+    expect(head).toBeInTheDocument();
+  });
+});
